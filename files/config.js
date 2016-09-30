3114d877a3f3182615e7fc951be1daefdf00499e
Sanjay Joshi <joshisa@us.ibm.com>
2016-09-29 22:09:32 -0400

    Update for support of Ghost 0.11.0

--- files/config.js
+++ files/config.js
@@ -6,6 +6,7 @@ var path = require('path'),
 
 // Modifications for BlueMix compatibility
 var postCreds;
+var cloudantCreds;
 var bluemixport = (process.env.VCAP_APP_PORT || '2368');
 var bluemixhost = (process.env.VCAP_APP_HOST || '127.0.0.1');
 var yaml = require('js-yaml');
@@ -33,6 +34,10 @@ if (process.env.VCAP_SERVICES) {
             postCreds = services[svcName][0]['credentials'];
             postCreds.client = 'mysql';
             postCreds.filename = '';
+        } else if (svcName.match(/^cloudantNoSQLDB/)) {
+            cloudantCreds = services[svcName][0]['credentials'];
+            cloudantCreds.client = 'cloudant';
+            cloudantCreds.filename = '';
         }
     }
 } else {
@@ -52,10 +57,16 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://my_app_name.ng.bluemix.net',
+        // Change this to your Ghost blog's published URL.
+        url: 'http://localhost:2368',
+
+        // Example refferer policy
+        // Visit https://www.w3.org/TR/referrer-policy/ for instructions
+        // default 'origin-when-cross-origin',
+        // referrerPolicy: 'origin-when-cross-origin',
 
         // Example mail config
-        // Visit http://docs.ghost.org/mail for instructions
+        // Visit http://support.ghost.org/mail for instructions
         // ```
         //  mail: {
         //      transport: 'SMTP',
@@ -69,6 +80,8 @@ config = {
         //  },
         // ```
 
+        // #### Database
+        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
         database: {
             client: 'sqlite3',
             connection: {
@@ -76,11 +89,18 @@ config = {
             },
             debug: false
         },
+        // #### Server
+        // Can be host & port (default), or socket
         server: {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
+        },
+        // #### Paths
+        // Specify where your content directory lives
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
         }
     },
 
@@ -90,7 +110,7 @@ config = {
     production: {
     	// URL constructed from data within the manifest.yml file.
         url: appurl,
-        
+
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
         // ```
@@ -105,7 +125,7 @@ config = {
         //      }
         //  },
         // ```
-        
+
         database: {
             client: postCreds.client,
             connection: {
@@ -124,7 +144,100 @@ config = {
             host: bluemixhost,
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: bluemixport
+        },
+        storage: {
+            active: 'cloudant',
+            'cloudant': {
+                host: cloudantCreds.host,
+                password: cloudantCreds.password,
+                port: cloudantCreds.port,
+                url: cloudantCreds.url,
+                username: cloudantCreds.username
+            }
+        },
+        mail: {
+            transport: 'SMTP',
+            from: '"Bluemix Ghost Blogger" <create_new_account@gmail.com>',
+            options: {
+                service: 'Gmail',
+                auth: {
+                    // http://support.ghost.org/mail/
+                    // https://accounts.google.com/SignUp
+                    user: 'create_new_account@gmail.com',
+                    pass: 'newpassword'
+                }
+            }
         }
+    },
+    // **Developers only need to edit below here**
+
+    // ### Testing
+    // Used when developing Ghost to run tests and check the health of Ghost
+    // Uses a different port number
+    testing: {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-test.db')
+            },
+            pool: {
+                afterCreate: function (conn, done) {
+                    conn.run('PRAGMA synchronous=OFF;' +
+                    'PRAGMA journal_mode=MEMORY;' +
+                    'PRAGMA locking_mode=EXCLUSIVE;' +
+                    'BEGIN EXCLUSIVE; COMMIT;', done);
+                }
+            },
+            useNullAsDefault: true
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing MySQL
+    // Used by Travis - Automated testing run through GitHub
+    'testing-mysql': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'mysql',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'root',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing pg
+    // Used by Travis - Automated testing run through GitHub
+    'testing-pg': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'pg',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'postgres',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
     }
 };
 
--- files/core/server/storage/cloudant.js
+++ files/core/server/storage/cloudant.js
@@ -1,21 +1,22 @@
 // # Local File System Image Storage module
 // The (default) module for storing images, using the local file system
 
-var _       = require('lodash'),
-    express = require('express'),
-    fs      = require('fs-extra'),
-    nodefn  = require('when/node/function'),
-    path    = require('path'),
-    when    = require('when'),
-    errors  = require('../errors'),
-    config  = require('../config'),
-    util     = require('util'),
-    Promise   = require('bluebird'),
-    baseStore   = require('./base'),
+var serveStatic = require('express').static,
+    fs = require('fs-extra'),
+    os = require('os'),
+    path = require('path'),
+    util = require('util'),
+    Promise = require('bluebird'),
+    errors = require('../errors'),
+    config = require('../config'),
+    utils = require('../utils'),
+    BaseStore = require('./base'),
+    remove = Promise.promisify(fs.remove),
     https =  require('https'),
     querystring = require('querystring'),
     dbname = 'ghost-images',
     imagestore,
+    cloudantCreds,
     cloudantFileStore;
 
 if (process.env.VCAP_APPLICATION) {
@@ -25,101 +26,105 @@ if (process.env.VCAP_APPLICATION) {
 
 if (process.env.VCAP_SERVICES) {
     var services = JSON.parse(process.env.VCAP_SERVICES);
-    // look for a service starting with 'CloudantNoSQLDB'
-    //
     for (var svcName in services) {
         if (svcName.match(/^cloudantNoSQLDB/)) {
             cloudantCreds = services[svcName][0]['credentials'];
         }
     }
-} else {
-    console.log('No Cloudant CouchDB service attached');
 }
-
-var nano = require('nano')(cloudantCreds.url);
-
-nano.db.get(dbname, function(err, body) {
-    if (err) {
-        nano.db.create(dbname, function(err, body) {
-            if (!err) {
-                console.log(dbname.toUpperCase() + ' database was successfully created');
-                // Using the format  https://<username>:<password>@cloudant.com/api/set_permissions
-                // Content-Type:  application/x-www-form-urlencoded
-                // Post Body:  username=nobody&database=<username>/<dbname>&roles=_reader
-                // The username "nobody" represents the "Everone Else" unauthenticated user group (aka Public)
-
-                var data = querystring.stringify({
-                    username: 'nobody',
-                    database: cloudantCreds.username + '/' + dbname,
-                    roles: '_reader'
-                });
-
-                var auth = 'Basic ' + new Buffer( cloudantCreds.username + ':' + cloudantCreds.password).toString('base64');
-
-                var options = {
-                    hostname: 'cloudant.com',
-                    port: 443,
-                    path: '/api/set_permissions',
-                    method: 'POST',
-                    headers: {
-                        'Authorization' : auth,
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Content-Length': Buffer.byteLength(data)
-                    }
-                };
-
-                console.log('Setting permissions to reader on ' + dbname.toUpperCase() + ' for unauthenticated users ...');
-                var request=https.request(options, function(response) {
-                    response.setEncoding('utf8');
-                    response.on('data', function (chunk) {
-                        if (JSON.parse(chunk).ok === true) {
-                        //if (chunk.indexOf('ok') >= 0) {
-                            console.log("Permissions successfully set.");
-                        } else {
-                            console.log("Permissions fail to set");
-                            console.log(JSON.parse.ok);
-                            console.log('Body: ' + chunk);
-                        }
-                    });
-                });
-                request.write(data);
-                request.end();
-                imagestore = nano.use(dbname);
-            } else {
-                console.log(dbname.toUpperCase() + ' database has failed to be created');
-                console.log('Reason: ' + err.error);
-            }
-        });
-    } else {
-        console.log(dbname.toUpperCase() + ' database already exists and will be used.');
-        imagestore = nano.use(dbname);
-    }
-});
 
 function CloudantFileStore() {
+    BaseStore.call(this);
+    var options = {};
+
+    options.host = cloudantCreds.host || '';
+    options.password = cloudantCreds.password || '';
+    options.port = cloudantCreds.port || 443;
+    options.url = cloudantCreds.url || '';
+    options.username = cloudantCreds.username || '';
+    if (!options.url) {
+        console.error('ERROR: No Cloudant connection string url found!');
+    } else {
+      var nano = require('nano')(options.url);
+      nano.db.get(dbname, function(err, body) {
+          if (err) {
+              nano.db.create(dbname, function(err, body) {
+                  if (!err) {
+                      console.log(dbname.toUpperCase() + ' database was successfully created');
+                      // Using the format  https://<username>:<password>@cloudant.com/api/set_permissions
+                      // Content-Type:  application/x-www-form-urlencoded
+                      // Post Body:  username=nobody&database=<username>/<dbname>&roles=_reader
+                      // The username "nobody" represents the "Everone Else" unauthenticated user group (aka Public)
+
+                      var data = querystring.stringify({
+                          username: 'nobody',
+                          database: cloudantCreds.username + '/' + dbname,
+                          roles: '_reader'
+                      });
+
+                      var auth = 'Basic ' + new Buffer( cloudantCreds.username + ':' + cloudantCreds.password).toString('base64');
+
+                      var options = {
+                          hostname: 'cloudant.com',
+                          port: 443,
+                          path: '/api/set_permissions',
+                          method: 'POST',
+                          headers: {
+                              'Authorization' : auth,
+                              'Content-Type': 'application/x-www-form-urlencoded',
+                              'Content-Length': Buffer.byteLength(data)
+                          }
+                      };
+
+                      console.log('Setting permissions to reader on ' + dbname.toUpperCase() + ' for unauthenticated users ...');
+                      var request=https.request(options, function(response) {
+                          response.setEncoding('utf8');
+                          response.on('data', function (chunk) {
+                              if (JSON.parse(chunk).ok === true) {
+                              //if (chunk.indexOf('ok') >= 0) {
+                                  console.log("Permissions successfully set.");
+                              } else {
+                                  console.log("Permissions fail to set");
+                                  console.log(JSON.parse.ok);
+                                  console.log('Body: ' + chunk);
+                              }
+                          });
+                      });
+                      request.write(data);
+                      request.end();
+                      imagestore = nano.use(dbname);
+                  } else {
+                      console.log(dbname.toUpperCase() + ' database has failed to be created');
+                      console.log('Reason: ' + err.error);
+                  }
+              });
+          } else {
+              console.log(dbname.toUpperCase() + ' database already exists and will be used.');
+              imagestore = nano.use(dbname);
+          }
+      });
+    }
 }
-util.inherits(CloudantFileStore, baseStore);
+
+util.inherits(CloudantFileStore, BaseStore);
 
 // ### Save
 // Saves the image to storage (the file system)
 // - image is the express image object
 // - returns a promise which ultimately returns the full url to the uploaded image
-CloudantFileStore.prototype.save = function (image) {
-    var saved = when.defer(),
-        targetDir = this.getTargetDir(config.get().paths.imagesPath),
-        targetFilename;
+CloudantFileStore.prototype.save = function (image, targetDir) {
+    var targetDir = targetDir || this.getTargetDir(config.paths.imagesPath);
+    var targetFilename = 'fakefile';
 
-    this.getUniqueFileName(this, image, targetDir).then(function (filename) {
+    return this.getUniqueFileName(this, image, targetDir).then(function (filename) {
         targetFilename = filename;
-        return nodefn.call(fs.mkdirs, targetDir);
+        return Promise.promisify(fs.mkdirs)(targetDir);
     }).then(function () {
-        return nodefn.call(fs.copy, image.path, targetFilename);
-    }).then(function () {
-        return nodefn.call(fs.unlink, image.path).otherwise(errors.logError);
+        return Promise.promisify(fs.copy)(image.path, targetFilename);
     }).then(function () {
         // The src for the image must be in URI format, not a file system path, which in Windows uses \
         // For local file system storage can use relative path so add a slash
-        var fullUrl = (config.get().paths.subdir + '/' + path.relative(config.get().paths.appRoot, targetFilename)).replace(new RegExp('\\' + path.sep, 'g'), '/');
+        var fullUrl = (config.paths.subdir + '/' + path.relative(config.paths.appRoot, targetFilename)).replace(new RegExp('\\' + path.sep, 'g'), '/');
         console.log("Full Local URL: " + fullUrl)
         // Let's assume PNG images are the norm and toggle the content-type from there
         var contentType = 'image/png';
@@ -144,8 +149,8 @@ CloudantFileStore.prototype.save = function (image) {
 
         var base = path.basename(targetFilename, extension);
         //console.log(cloudantCreds.url + '/' + dbname + '/' + base + fullUrl);
-        var cloudantImageUrl = cloudantCreds.url + '/' + dbname + '/' + base + fullUrl;
-
+        var cloudantImageUrl = 'https://' + cloudantCreds.host + '/' + dbname + '/' + base + fullUrl;
+        console.log('Cloudant URL: ' + cloudantImageUrl);
         //Let's try to find a document with the same label as the image and then drill down to find the image attachment
         imagestore.get(base, {revs_info: true}, function(err, getBody) {
             if (err) {
@@ -212,60 +217,99 @@ CloudantFileStore.prototype.save = function (image) {
         });
 
         // Let's give Cloudant a wee bit of time to get the data and create the doc + attachment.  Say 1000 ms seems like a good first approx.
-        setTimeout(function() {return saved.resolve(cloudantImageUrl);}, 1000);
+        return cloudantImageUrl;
 
-    }).otherwise(function (e) {
+    }).catch(function (e) {
         errors.logError(e);
-        return saved.reject(e);
+        return Promise.reject(e);
     });
-
-    return saved.promise;
-}
+};
 
 CloudantFileStore.prototype.exists = function (filename) {
-    // fs.exists does not play nicely with nodefn because the callback doesn't have an error argument
-    var done = when.defer();
-    var extension = path.extname(filename);
-    var base = path.basename(filename, extension);
-    var fullname = base + extension;
-    var fullUrl = (config.get().paths.subdir + '/' + path.relative(config.get().paths.appRoot, filename)).replace(new RegExp('\\' + path.sep, 'g'), '/');
+    return new Promise(function (resolve) {
+       // fs.exists does not play nicely with nodefn because the callback doesn't have an error argument
+      var extension = path.extname(filename);
+      var base = path.basename(filename, extension);
+      var fullname = base + extension;
+      var fullUrl = (config.paths.subdir + '/' + path.relative(config.paths.appRoot, filename)).replace(new RegExp('\\' + path.sep, 'g'), '/');
 
-    // Let's try to get this image from Cloudant by checking for the doc first
-    imagestore.get(base, {revs_info: true}, function(err, getBody) {
-        if (err) {
-            // Couldn't find the doc.  Record the reason and notify as false.
-            console.log('{EXISTS} Could not find the doc: ' + base);
-            console.log('Reason: ' + err.error);
-            done.resolve(false);
-        } else {
-            console.log('{EXISTS} Found image document in Cloudant cache: ' + base);
-            // Let's try to get the image attachment associated with this doc
-            imagestore.attachment.get(base, fullUrl, function (err, attachBody) {
-                if (!err) {
-                    // We found it.  Notify as true.
-                    console.log('{EXISTS} Found image attachment: ' + fullUrl)
-                    done.resolve(false);    // Let's fall through the same code as not being there.
-                } else {
-                    // Not there.  Record the reason and notify as false
-                    console.log('{EXISTS} Could not find the image ' + fullUrl);
-                    console.log('Reason: ' + err.error);
-                    done.resolve(false);
-                }
-            });
-        }
-    });
-
-    return done.promise;
-}
+      // Let's try to get this image from Cloudant by checking for the doc first
+      imagestore.get(base, {revs_info: true}, function(err, getBody) {
+          if (err) {
+              // Couldn't find the doc.  Record the reason and notify as false.
+              console.log('{EXISTS} Could not find the doc: ' + base);
+              console.log('Reason: ' + err.error);
+              resolve(false);
+          } else {
+              console.log('{EXISTS} Found image document in Cloudant cache: ' + base);
+              // Let's try to get the image attachment associated with this doc
+              imagestore.attachment.get(base, fullUrl, function (err, attachBody) {
+                  if (!err) {
+                      // We found it.  Notify as true.
+                      console.log('{EXISTS} Found image attachment: ' + fullUrl)
+                      resolve(true);    // Let's fall through the same code as not being there.
+                  } else {
+                      // Not there.  Record the reason and notify as false
+                      console.log('{EXISTS} Could not find the image ' + fullUrl);
+                      console.log('Reason: ' + err.error);
+                      resolve(false);
+                  }
+              });
+          }
+      });
+   });
+};
 
 // middleware for serving the files
-CloudantFileStore.prototype.serve = function () {
-    var ONE_HOUR_MS = 60 * 60 * 1000,
-        ONE_YEAR_MS = 365 * 24 * ONE_HOUR_MS;
+CloudantFileStore.prototype.serve = function (options) {
+    options = options || {};
 
-    // For some reason send divides the max age number by 1000
-    return express.static(config.get().paths.imagesPath, {maxAge: ONE_YEAR_MS});
-}
+    // CASE: serve themes
+    // serveStatic can't be used to serve themes, because
+    // download files depending on the route (see `send` npm module)
+    if (options.isTheme) {
+        return function downloadTheme(req, res, next) {
+            var themeName = options.name,
+                themePath = path.join(config.paths.themePath, themeName),
+                zipName = themeName + '.zip',
+                // store this in a unique temporary folder
+                zipBasePath = path.join(os.tmpdir(), utils.uid(10)),
+                zipPath = path.join(zipBasePath, zipName),
+                stream;
+
+            Promise.promisify(fs.ensureDir)(zipBasePath)
+                .then(function () {
+                    return Promise.promisify(utils.zipFolder)(themePath, zipPath);
+                })
+                .then(function (length) {
+                    res.set({
+                        'Content-disposition': 'attachment; filename={themeName}.zip'.replace('{themeName}', themeName),
+                        'Content-Type': 'application/zip',
+                        'Content-Length': length
+                    });
+
+                    stream = fs.createReadStream(zipPath);
+                    stream.pipe(res);
+                })
+                .catch(function (err) {
+                    next(err);
+                })
+                .finally(function () {
+                    remove(zipBasePath);
+                });
+        };
+    } else {
+        // CASE: serve images
+        // For some reason send divides the max age number by 1000
+        // Fallthrough: false ensures that if an image isn't found, it automatically 404s
+        return serveStatic(config.paths.imagesPath, {maxAge: utils.ONE_YEAR_MS, fallthrough: false});
+    }
+};
+
+CloudantFileStore.prototype.delete = function (fileName, targetDir) {
+    targetDir = targetDir || this.getTargetDir(config.paths.imagesPath);
+    var pathToDelete = path.join(targetDir, fileName);
+    return remove(pathToDelete);
+};
 
 module.exports = CloudantFileStore;
-
